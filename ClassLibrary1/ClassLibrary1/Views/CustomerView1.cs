1cf21b596f775c8e60561a4a915e25d0595bf1bf
MihA_aa <MihA_aa@mail.ru>
2017-04-27 21:31:37 +0300

    Make views

---
+++ ClassLibrary1/ClassLibrary1/Views/CustomerView1.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Views
+{
+    class CustomerView1
+    {
+        public virtual Customer Customer { get; set; }
+        public string Name { get; set; }
+        public int DisplayIndex { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public int Quantity { get; set; }
+        public decimal PercentWins { get; set; }
+        public decimal BiggestWinner { get; set; }
+        public decimal BiggestLoser { get; set; }
+        public decimal AvgGain { get; set; }
+        public decimal MonthAvgGain { get; set; }
+        public decimal PortfolioValue { get; set; }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/Views/CustomerView2.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Views
+{
+    class CustomerView2
+    {
+        public virtual Customer Customer { get; set; }
+        public string Name { get; set; }
+        public int DisplayIndex { get; set; }
+        public int Quantity { get; set; }
+        public decimal PercentWins { get; set; }
+        public decimal BiggestWinner { get; set; }
+        public decimal PortfolioValue { get; set; }
+    }
+}
